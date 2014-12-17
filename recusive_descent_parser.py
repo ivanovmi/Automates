9c1300b63dced71f74505db64a52c3bfe2d8f2cc
Michael Ivanov <difmel@gmail.com>
2014-12-17 10:22:59 +0300

    recursive descent, parse the string with counter from file

---
+++ recusive_descent_parser.py
@@ -0,0 +1,17 @@
+__author__ = 'michael'
+
+import pyparsing
+
+f = open("pars.txt", 'r')
+count = int(f.readline())
+
+variable = pyparsing.Word(pyparsing.alphas + pyparsing.nums)
+fraction = pyparsing.nums[1] + pyparsing.Literal("/") + variable
+pars_String = pyparsing.nums[1]
+while count != 0:
+    pars_String = pars_String + pyparsing.Literal("+") + fraction
+    count -= 1
+
+test = "1+1/x1+1/x2+1/x4"
+
+print pars_String.parseString(test)

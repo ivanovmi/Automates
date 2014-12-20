bf74cf7b3cd65b37927cf476de879af3acb5c7d9
Evgeny Sikachov <jakasikachov@gmail.com>
2014-12-20 20:43:40 +0400

    Paste michael's strings

--- recusive_descent_parser.py
+++ recusive_descent_parser.py
@@ -6,10 +6,14 @@ count = int(f.readline())
 variable = pyparsing.Word(pyparsing.alphas + pyparsing.nums)
 fraction = pyparsing.nums[1] + pyparsing.Literal("/") + variable
 pars_String = pyparsing.nums[1]
+ex = pyparsing.Word(pyparsing.alphas + pyparsing.alphanums)
+num = pyparsing.nums
 while count != 0:
-    pars_String = pars_String + pyparsing.Literal("+") + fraction
+    pars = pyparsing.Literal("(") + num + pyparsing.Literal("*") + ex + pyparsing.Literal("+") + \
+           num +pyparsing.Literal(")") + pyparsing.Literal("/") + pyparsing.Literal("(") + num + \
+           pyparsing.Literal("*") + ex + pyparsing.Literal("+") + num +pyparsing.Literal(")")
     count -= 1
 
-test = "1+1/x1+1/x2+1/x4"
+function=raw_input("Enter the function: ")
 
-print pars_String.parseString(test)
+print pars_String.parseString(function)

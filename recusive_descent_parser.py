6c3e9d997d5d9bea32a0b14e65a940becef849dc
Evgeny Sikachov <jakasikachov@gmail.com>
2014-12-20 21:03:25 +0400

    Fix code

--- recusive_descent_parser.py
+++ recusive_descent_parser.py
@@ -1,19 +1,14 @@
 import pyparsing
 
-f = open("pars.txt", 'r')
-count = int(f.readline())
-
-variable = pyparsing.Word(pyparsing.alphas + pyparsing.nums)
-fraction = pyparsing.nums[1] + pyparsing.Literal("/") + variable
-pars_String = pyparsing.nums[1]
-ex = pyparsing.Word(pyparsing.alphas + pyparsing.alphanums)
+ex = pyparsing.Word(pyparsing.alphas)
 num = pyparsing.nums
-while count != 0:
-    pars = pyparsing.Literal("(") + num + pyparsing.Literal("*") + ex + pyparsing.Literal("+") + \
-           num +pyparsing.Literal(")") + pyparsing.Literal("/") + pyparsing.Literal("(") + num + \
-           pyparsing.Literal("*") + ex + pyparsing.Literal("+") + num +pyparsing.Literal(")")
-    count -= 1
 
-function=raw_input("Enter the function: ")
+pars_String = pyparsing.Literal("(") + num + pyparsing.Literal("*") + ex + pyparsing.Literal("+") + \
+                num +pyparsing.Literal(")") + pyparsing.Literal("/") + pyparsing.Literal("(") + num + \
+                pyparsing.Literal("*") + ex + pyparsing.Literal("+") + num +pyparsing.Literal(")")
 
+
+#function=raw_input("Enter the function: ")
+function="(5*x+4)/(4*x+5)"
 print pars_String.parseString(function)
+

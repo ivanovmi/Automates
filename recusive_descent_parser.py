15a398bf900eadc3c96572077ae1e0ef59e36b50
Evgeny Sikachov <jakasikachov@gmail.com>
2014-12-20 21:39:49 +0400

    Working code

--- recusive_descent_parser.py
+++ recusive_descent_parser.py
@@ -1,14 +1,30 @@
 import pyparsing
 
-ex = pyparsing.Word(pyparsing.alphas)
-num = pyparsing.nums
 
-pars_String = pyparsing.Literal("(") + num + pyparsing.Literal("*") + ex + pyparsing.Literal("+") + \
-                num +pyparsing.Literal(")") + pyparsing.Literal("/") + pyparsing.Literal("(") + num + \
-                pyparsing.Literal("*") + ex + pyparsing.Literal("+") + num +pyparsing.Literal(")")
+def create_string():
+    variable_of_function = pyparsing.Word(pyparsing.alphas)
+    number = pyparsing.Word(pyparsing.nums)
+
+    pars_string = pyparsing.Literal("(") + number + pyparsing.Literal("*") + variable_of_function + \
+                  pyparsing.Literal("+") + number + pyparsing.Literal(")") + pyparsing.Literal("/") + \
+                  pyparsing.Literal("(") + number + pyparsing.Literal("*") + variable_of_function + \
+                  pyparsing.Literal("+") + number + pyparsing.Literal(")")
+
+    # input_function=raw_input("Enter the input_function: ")
+    input_function = "(5*x+4)/(4*x+5)"
+    return pars_string.parseString(input_function)
 
 
-#function=raw_input("Enter the function: ")
-function="(5*x+4)/(4*x+5)"
-print pars_String.parseString(function)
+def generate_of_run_file():
+    file_for_runinning.write("from __future__ import division\n")
+    file_for_runinning.write("variable = int(raw_input('Enter the value of variable: '))\n")
+    file_for_runinning.write("a1 = %s\n" % create_string()[1])
+    file_for_runinning.write("a2 = %s\n" % create_string()[5])
+    file_for_runinning.write("b1 = %s\n" % create_string()[9])
+    file_for_runinning.write("b2 = %s\n" % create_string()[13])
+    file_for_runinning.write("print (a1*variable+a2)/(b1*variable+b2)")
 
+
+file_for_runinning = open("run_me.py", "w")
+generate_of_run_file()
+file_for_runinning.close()
---
+++ run_me.py
@@ -0,0 +1,7 @@
+from __future__ import division
+variable = int(raw_input('Enter the value of variable: '))
+a1 = 5
+a2 = 4
+b1 = 4
+b2 = 5
+print (a1*variable+a2)/(b1*variable+b2)

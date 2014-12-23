15ee7da810a417633cb2f730648fb8c65b35f413
Evgeny Sikachev <esikachev@mirantis.com>
2014-12-23 21:35:56 +0400

    Finished

--- recusive_descent_parser.py
+++ recusive_descent_parser.py
@@ -4,28 +4,32 @@ import pyparsing
 def create_string():
     name_of_array = pyparsing.Word(pyparsing.alphas)
     number = pyparsing.Word(pyparsing.nums)
-
-
-    pars_string = name_of_array + pyparsing.Literal("[") + number + pyparsing.Literal("]") + pyparsing.Literal(
-        "[") + number + pyparsing.Literal("]")
-
     input_file = open("input", "r")
-    input_function = input_file.readline()
-
+    input_data = input_file.readline()
     input_file.close()
-    return pars_string.parseString(input_function)
+    parse_input = []
+    arrays=[]
+    address = input_data.split(";")
+    for i in address:
+        pars_string = name_of_array + pyparsing.Literal("[") + number + pyparsing.Literal("]") + \
+                      pyparsing.Literal("[") + number + pyparsing.Literal("]")
+        parse_input.append(pars_string.parseString(i))
+    for i in parse_input:
+        if i[0] not in arrays:
+            arrays.append(i[0])
+    generate_of_run_file(address,arrays)
 
 
-def generate_of_run_file():
-    file_for_runinning.write("from __future__ import division\n")
-    file_for_runinning.write("variable = int(raw_input('Enter the value of variable: '))\n")
-    file_for_runinning.write("a1 = %s\n" % create_string()[1])
-    file_for_runinning.write("a2 = %s\n" % create_string()[5])
-    file_for_runinning.write("b1 = %s\n" % create_string()[9])
-    file_for_runinning.write("b2 = %s\n" % create_string()[13])
-    file_for_runinning.write("print (a1*variable+a2)/(b1*variable+b2)")
+def generate_of_run_file(array,arrays):
+    file_for_runnining.write("from random import randint\n")
+    for i in arrays:
+        file_for_runnining.write("%s = [[0]*10]*10\n" % i)
+    for i in array:
+        file_for_runnining.write("%s = randint(0,100)\n" % i)
+    for i in arrays:
+        file_for_runnining.write("print %s\n" % i)
 
 
-file_for_runinning = open("run_me.py", "w")
-generate_of_run_file()
-file_for_runinning.close()
+file_for_runnining = open("run_me.py", "w")
+create_string()
+file_for_runnining.close()
--- run_me.py
+++ run_me.py
@@ -1,7 +1,12 @@
-from __future__ import division
-variable = int(raw_input('Enter the value of variable: '))
-a1 = 5
-a2 = 43
-b1 = 10
-b2 = 9
-print (a1*variable+a2)/(b1*variable+b2)
+from random import randint
+a = [[0]*10]*10
+b = [[0]*10]*10
+c = [[0]*10]*10
+a[1][0] = randint(0,100)
+a[1][1] = randint(0,100)
+a[0][0] = randint(0,100)
+b[0][0] = randint(0,100)
+c[0][0] = randint(0,100)
+print a
+print b
+print c
